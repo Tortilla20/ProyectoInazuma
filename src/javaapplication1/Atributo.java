6fec6833e16dbb0095622ea7c045c4b724b7e1c0
ChristianCastro-dev <christiancastroiglesias@gmail.com>
2026-01-14 12:27:44 +0100

    primer commit

---
+++ src/javaapplication1/Atributo.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author dam2_alu02@inf.ald
+ */
+class Atributo {
+    private int id;
+    private int 
+}
